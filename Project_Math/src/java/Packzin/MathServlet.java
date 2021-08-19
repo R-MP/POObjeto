7b86087de2db7ac605b8518267745a8ba738bbc2
Renan MP <renanmp2015@gmail.com>
2021-08-19 18:41:03 -0300

    Update MathServlet.java

--- Project_Math/src/java/Packzin/MathServlet.java
+++ Project_Math/src/java/Packzin/MathServlet.java
@@ -42,11 +42,12 @@ public class MathServlet extends HttpServlet {
             out.println("<h4><a href='index.html'>Voltar</a></h4>");
             out.println("<h1>Operações Matematicas</h1>");
             out.println("<form action='math-servlet.html'>"+"Continue as operações colocando os números aqui:<br>\n"+"<input type=\"number\" name=\"x\"/>\n"+"e "+"<input type=\"number\" name=\"y\"/>\n"+"<input type=\"submit\" value=\"Calcular Tudo\"/>\n"+"</form>");
-            int x = 0;
-            int y = 0;
+            double x = 0;
+            double y = 0;
+            
             try{
-                x = Integer.parseInt(request.getParameter("x"));
-                y = Integer.parseInt(request.getParameter("y"));
+                x = Double.parseDouble(request.getParameter("x"));
+                y = Double.parseDouble(request.getParameter("y"));
             }catch(NumberFormatException ex){
             out.println("<h5><p style='color:blue'>Favor informar o primeiro e o segundo número</p></h3>");
             }
